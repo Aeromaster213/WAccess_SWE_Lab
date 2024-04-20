96c55556aacd9a55299033f45f334a2cf7dd75c8
arpit <arpitb.lgupta@gmail.com>
2024-04-20 22:47:05 +0530

    change tag

--- 2_2_2_Pause,Stop,Hide(A).js
+++ 2_2_2_Pause,Stop,Hide(A).js
@@ -1,5 +1,5 @@
 setTimeout(() => {
-    Pause_Stop_Hide()
+    Pause_Stop_Hide();
 }, 11000);
 
 function Pause_Stop_Hide() {
@@ -7,70 +7,22 @@ function Pause_Stop_Hide() {
         console.log.apply(console, this);
         return this;
     };
-    var marqueeTags = document.querySelectorAll('marquee')
+
+    var marqueeTags = document.querySelectorAll('marquee');
     for (var d = 0; d < marqueeTags.length; d++) {
-        console.log("%cRule:%cWCAG 2.2.2 (2.0,A)",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px 0px 0px 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-                            display: inline;
-                            font-size: 0.8rem;
-                            background-color: #809FFF;
-                            border-radius: 0px 5px 5px 0px;
-                            padding: 5px 10px;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cError:%cMarquee tag found in the html page",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px 0px 0px 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-                            display: inline;
-                            font-size: 0.8rem;
-                            background-color: #EB5177;
-                            border-radius: 0px 5px 5px 0px;
-                            padding: 5px 10px;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cCode Snippet:",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        $(marqueeTags[d]).log()
-        console.log("%cFix:%cProvide users enough time to read and use content. Use strong or em tag instead of marquee.",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px 0px 0px 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-                            display: inline;
-                            font-size: 0.8rem;
-                            background-color: #007075;
-                            border-radius: 0px 5px 5px 0px;
-                            padding: 5px 10px;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%c-----------------------------------------------------------------------------",
-            `color: #FFF;
-                            background-color: #293543;
-                            font-weight: bolder;
-                            border-radius: 5px;
-                            padding: 5px 10px;
-                            font-size: 1rem;
-                            display: inline;`)
+        // Log the violation
+        console.log("%cRule:%cWCAG 2.2.2 (2.0,A)", ruleStyle, infoStyle);
+        console.log("%cError:%cMarquee tag found in the html page", ruleStyle, errorStyle);
+        console.log("%cCode Snippet:", codeSnippetStyle);
+        $(marqueeTags[d]).log();
+        console.log("%cFix:%cProvide users enough time to read and use content. Use strong or em tag instead of marquee.", ruleStyle, fixStyle);
+        console.log("%c-----------------------------------------------------------------------------", separatorStyle);
+
+        // Replace marquee tag with strong or em tag
+        var parentElement = marqueeTags[d].parentNode;
+        var textContent = marqueeTags[d].textContent;
+        var replacementTag = document.createElement('strong'); // or 'em' depending on the semantics
+        replacementTag.textContent = textContent;
+        parentElement.replaceChild(replacementTag, marqueeTags[d]);
     }
 }
-
