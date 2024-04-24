26b5dfd916aaf49d93f2dfe2870de5304b7703dd
Keshavv7 <cs21b027@iittp.ac.in>
2024-04-24 13:19:35 +0530

    Fixing 1_3_1 and 1_4_4

--- 1_4_4_ResizeText(AA).js
+++ 1_4_4_ResizeText(AA).js
@@ -1,3 +1,4 @@
+
 setTimeout(() => {
     ResizeText()
 }, 8000);
@@ -7,214 +8,26 @@ function ResizeText() {
         console.log.apply(console, this);
         return this;
     };
-    var italicTags = document.querySelectorAll('i')
-    for (var d = 0; d < italicTags.length; d++) {
-        console.log("%cRule:%cWCAG 1.4.4 (2.0,AA)",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px 0px 0px 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-                            display: inline;
-                            font-size: 0.8rem;
-                            background-color: #809FFF;
-                            border-radius: 0px 5px 5px 0px;
-                            padding: 5px 10px;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cError:%cFound italic tag",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px 0px 0px 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-                            display: inline;
-                            font-size: 0.8rem;
-                            background-color: #EB5177;
-                            border-radius: 0px 5px 5px 0px;
-                            padding: 5px 10px;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cCode Snippet:",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        $(italicTags[d]).log()
-        var newTag = document.createElement("em");
-        newTag.innerHTML = italicTags[d].innerHTML;
-        italicTags[d].replaceWith(newTag);
-        console.log("%cFix:%cInstead use strong or em tag",
-            `color: #FFF;
-                            background-color: #333;
-                            border-radius: 5px 0px 0px 5px;
-                            padding: 5px 10px;
-                            font-size: 0.8rem;
-                            display: inline;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-                            display: inline;
-                            font-size: 0.8rem;
-                            background-color: #007075;
-                            border-radius: 0px 5px 5px 0px;
-                            padding: 5px 10px;
-                            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%c-----------------------------------------------------------------------------",
-            `color: #FFF;
-                            background-color: #293543;
-                            font-weight: bolder;
-                            border-radius: 5px;
-                            padding: 5px 10px;
-                            font-size: 1rem;
-                            display: inline;`)
-    }
-    var boldTags = document.querySelectorAll('bold')
-    for (var d = 0; d < boldTags.length; d++) {
-        console.log("%cRule:%cWCAG 1.4.4 (2.0,AA)",
-            `color: #FFF;
-            background-color: #333;
-            border-radius: 5px 0px 0px 5px;
-            padding: 5px 10px;
-            font-size: 0.8rem;
-            display: inline;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-            display: inline;
-            font-size: 0.8rem;
-            background-color: #809FFF;
-            border-radius: 0px 5px 5px 0px;
-            padding: 5px 10px;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cError:%cFound bold tag",
-            `color: #FFF;
-            background-color: #333;
-            border-radius: 5px 0px 0px 5px;
-            padding: 5px 10px;
-            font-size: 0.8rem;
-            display: inline;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-            display: inline;
-            font-size: 0.8rem;
-            background-color: #EB5177;
-            border-radius: 0px 5px 5px 0px;
-            padding: 5px 10px;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cCode Snippet:",
-            `color: #FFF;
-            background-color: #333;
-            border-radius: 5px;
-            padding: 5px 10px;
-            font-size: 0.8rem;
-            display: inline;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        $(boldTags[d]).log()
-        var newTag = document.createElement("strong");
-        newTag.innerHTML = boldTags[d].innerHTML;
-        boldTags[d].replaceWith(newTag);
-        console.log("%cFix:%cInstead use strong or em tag",
-            `color: #FFF;
-            background-color: #333;
-            border-radius: 5px 0px 0px 5px;
-            padding: 5px 10px;
-            font-size: 0.8rem;
-            display: inline;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-            display: inline;
-            font-size: 0.8rem;
-            background-color: #007075;
-            border-radius: 0px 5px 5px 0px;
-            padding: 5px 10px;
-            box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%c-----------------------------------------------------------------------------",
-            `color: #FFF;
-            background-color: #293543;
-            font-weight: bolder;
-            border-radius: 5px;
-            padding: 5px 10px;
-            font-size: 1rem;
-            display: inline;`)
 
+    // Handle italic tags
+    const italicTags = document.querySelectorAll('i');
+    for (let d = 0; d < italicTags.length; d++) {
+        window.errorMessage("WCAG 1.4.4 (2.0,AA)", "Found italic tag", "Instead use strong or em tag", italicTags[d]);
     }
 
-    var fontTags = document.querySelectorAll('font')
-    for (var d = 0; d < fontTags.length; d++) {
-        console.log("%cRule:%cWCAG 1.4.4 (2.0,AA)",
-            `color: #FFF;
-             background-color: #333;
-             border-radius: 5px 0px 0px 5px;
-             padding: 5px 10px;
-             font-size: 0.8rem;
-             display: inline;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-             display: inline;
-             font-size: 0.8rem;
-             background-color: #809FFF;
-             border-radius: 0px 5px 5px 0px;
-             padding: 5px 10px;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cError:%cFound font tag",
-            `color: #FFF;
-             background-color: #333;
-             border-radius: 5px 0px 0px 5px;
-             padding: 5px 10px;
-             font-size: 0.8rem;
-             display: inline;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-             display: inline;
-             font-size: 0.8rem;
-             background-color: #EB5177;
-             border-radius: 0px 5px 5px 0px;
-             padding: 5px 10px;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%cCode Snippet:",
-            `color: #FFF;
-             background-color: #333;
-             border-radius: 5px;
-             padding: 5px 10px;
-             font-size: 0.8rem;
-             display: inline;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        $(fontTags[d]).log()
-        var newTag = document.createElement("p");
-        newTag.innerHTML = fontTags[d].innerHTML;
-        fontTags[d].replaceWith(newTag);
-        console.log("%cFix:%cRemove it. Avoid using it.",
-            `color: #FFF;
-             background-color: #333;
-             border-radius: 5px 0px 0px 5px;
-             padding: 5px 10px;
-             font-size: 0.8rem;
-             display: inline;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-            `color: #FFF;
-             display: inline;
-             font-size: 0.8rem;
-             background-color: #007075;
-             border-radius: 0px 5px 5px 0px;
-             padding: 5px 10px;
-             box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-        console.log("%c-----------------------------------------------------------------------------",
-            `color: #FFF;
-             background-color: #293543;
-             font-weight: bolder;
-             border-radius: 5px;
-             padding: 5px 10px;
-             font-size: 1rem;
-             display: inline;`)
-
+    // Handle bold tags
+    const boldTags = document.querySelectorAll('bold');
+    for (let d = 0; d < boldTags.length; d++) {
+        window.errorMessage("WCAG 1.4.4 (2.0,AA)", "Found bold tag", "Instead use strong or em tag", boldTags[d]);
     }
 
+    // Handle font tags
+    const fontTags = document.querySelectorAll('font');
+    for (let d = 0; d < fontTags.length; d++) {
+        window.errorMessage("WCAG 1.4.4 (2.0,AA)", "Found font tag", "Remove it. Avoid using it.", fontTags[d]);
+    }
+
+    // Handle other elements
     $(document).ready(function () {
         $('*').each(function () {
             if ($(this).prop("nodeName") == "DIV" ||
@@ -255,53 +68,11 @@ function ResizeText() {
                     textRoot += $(this).text();
                 });
                 if ($.trim(textRoot)) {
-                    console.log("%cRule:%cWCAG 1.4.4 (2.0,AA)",
-                        `color: #FFF;
-                         background-color: #333;
-                         border-radius: 5px 0px 0px 5px;
-                         padding: 5px 10px;
-                         font-size: 0.8rem;
-                         display: inline;
-                         box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-                        `color: #FFF;
-                         display: inline;
-                         font-size: 0.8rem;
-                         background-color: #809FFF;
-                         border-radius: 0px 5px 5px 0px;
-                         padding: 5px 10px;
-                         box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-                    console.log("%cSuggestion:%cPlease define width, height, and font-size for the elements that can contain text or take text input in `em`. This helps in resizing text.",
-                        `color: #FFF;
-                         background-color: #333;
-                         border-radius: 5px 0px 0px 5px;
-                         padding: 5px 10px;
-                         font-size: 0.8rem;
-                         display: inline;
-                         box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`,
-                        `color: #FFF;
-                         display: inline;
-                         font-size: 0.8rem;
-                         background-color: #007075;
-                         border-radius: 0px 5px 5px 0px;
-                         padding: 5px 10px;
-                         box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-                    console.log("%cCode Snippet:",
-                        `color: #FFF;
-                         background-color: #333;
-                         border-radius: 5px;
-                         padding: 5px 10px;
-                         font-size: 0.8rem;
-                         display: inline;
-                         box-shadow: 0 0 25px rgba(0, 0, 0, 0.05);`)
-                    $(this).log()
-                    console.log("%c-----------------------------------------------------------------------------",
-                        `color: #FFF;
-                         background-color: #293543;
-                         font-weight: bolder;
-                         border-radius: 5px;
-                         padding: 5px 10px;
-                         font-size: 1rem;
-                         display: inline;`)
+                    console.log(`%cRule:%cWCAG 1.4.4 (2.0,AA)`, window.ruleStyle, window.infoStyle);
+                    console.log(`%cSuggestion:%cPlease define width, height, and font-size for the elements that can contain text or take text input in 'em'. This helps in resizing text.`, window.errorStyle, window.fixStyle);
+                    console.log(`%cCode Snippet:`, window.codeSnippetStyle);
+                    $(this).log();
+                    console.log(`%c-----------------------------------------------------------------------------`, window.separatorStyle);
                 }
             }
         })
