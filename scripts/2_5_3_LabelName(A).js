4098ffbee72959bc006d04381e20204d0f7ff6a0
arpit <arpitb.lgupta@gmail.com>
2024-04-24 22:43:41 +0530

    Added New Rule

---
+++ scripts/2_5_3_LabelName(A).js
@@ -0,0 +1,33 @@
+// Ensure the label text is included in the accessible name
+function ensureLabelInName() {
+    var allComponents = document.querySelectorAll("[aria-labelledby]");
+    for (var i = 0; i < allComponents.length; i++) {
+        var component = allComponents[i];
+        var labelIds = component.getAttribute("aria-labelledby").split(" ");
+        var labelText = "";
+
+        // Retrieve text from labels
+        for (var j = 0; j < labelIds.length; j++) {
+            var labelElement = document.getElementById(labelIds[j]);
+            if (labelElement) {
+                var textContent = labelElement.textContent.trim();
+                if (textContent) {
+                    labelText += textContent + " ";
+                }
+            }
+        }
+
+        // Checks
+        if (labelText.trim() !== "") {
+            var accessibleName = component.textContent.trim();
+            if (!accessibleName.includes(labelText.trim())) {
+            
+                component.textContent = labelText + accessibleName;
+                window.errorMessage("WCAG 2.5.3 (A)", "Ensure label text is included in the accessible name", "Updated the accessible name", component);
+            }
+        }
+    }
+}
+
+// Call the function after a timeout
+setTimeout(ensureLabelInName, 17000);
